7810b904fe5812c56816111e70733eecefc0fc97
Jingchao Feng <fjch1997@outlook.com>
2019-10-01 14:34:35 -0400

    Registering done.

--- Homebridge/index.js
+++ Homebridge/index.js
@@ -1,5 +1,4 @@
-var http = require('http');
-var Accessory, Service, Characteristic, UUIDGen;
+var Accessory, Characteristic, UUIDGen;
 
 module.exports = function (homebridge) {
     console.log("homebridge API version: " + homebridge.version);
@@ -12,167 +11,36 @@ module.exports = function (homebridge) {
     Characteristic = homebridge.hap.Characteristic;
     UUIDGen = homebridge.hap.uuid;
 
-    // For platform plugin to be considered as dynamic platform plugin,
-    // registerPlatform(pluginName, platformName, constructor, dynamic), dynamic must be true
-    homebridge.registerPlatform("homebridge-markisol", "MarkisolPlatform", MarkisolPlatform, true);
+    homebridge.registerAccessory("homebridge-markisol", "MarkisolBlind", MarkisolAccessory);
 }
 
-class MarkisolPlatform {
-    constructor(log, config, api) {
+class MarkisolAccessory {
+    constructor(log, config) {
         this.log = log;
         this.config = config;
-        this.accessories = [];
-        this.api = api;
-        if (api) {
-            // Save the API object as plugin needs to register new accessory via this object
-
-            // Listen to event "didFinishLaunching", this means homebridge already finished loading cached accessories.
-            // Platform Plugin should only register new accessory that doesn't exist in homebridge after this event.
-            // Or start discover new accessories.
-            this.api.on('didFinishLaunching', function () {
-                platform.log("DidFinishLaunching");
-            }.bind(this));
+        const errorMessage = "Check your homebridge config.json. This accessory will not work.";
+        if (!this.config.name) {
+            this.log.error("A name for this MarkisolBlind accessory is not defined. " + errorMessage);
+        }
+        if (!this.config.remoteId) {
+            this.log.error("remoteId not defined for " + this.config.name + ". " + errorMessage);
+        }
+        if (!this.config.channel) {
+            this.log.error("channel not defined for " + this.config.name + ". " + errorMessage);
         }
     }
-    // Function invoked when homebridge tries to restore cached accessory.
-    // Developer can configure accessory at here (like setup event handler).
-    // Update current value.
-    configureAccessory(accessory) {
-        this.log(accessory.displayName, "Configure Accessory");
-        var platform = this;
-
-        // Set the accessory to reachable if plugin can currently process the accessory,
-        // otherwise set to false and update the reachability later by invoking 
-        // accessory.updateReachability()
-        accessory.reachable = true;
-
-        accessory.on('identify', function (paired, callback) {
-            platform.log(accessory.displayName, "Identify!!!");
-            callback();
-        });
-
-        if (accessory.getService(Service.Lightbulb)) {
-            accessory.getService(Service.Lightbulb)
-                .getCharacteristic(Characteristic.On)
-                .on('set', function (value, callback) {
-                    platform.log(accessory.displayName, "Light -> " + value);
-                    callback();
-                });
-        }
-
-        this.accessories.push(accessory);
-    }
-    // Handler will be invoked when user try to config your plugin.
-    // Callback can be cached and invoke when necessary.
-    configurationRequestHandler(context, request, callback) {
-        this.log("Context: ", JSON.stringify(context));
-        this.log("Request: ", JSON.stringify(request));
-
-        // Check the request response
-        if (request && request.response && request.response.inputs && request.response.inputs.name) {
-            this.addAccessory(request.response.inputs.name);
-
-            // Invoke callback with config will let homebridge save the new config into config.json
-            // Callback = function(response, type, replace, config)
-            // set "type" to platform if the plugin is trying to modify platforms section
-            // set "replace" to true will let homebridge replace existing config in config.json
-            // "config" is the data platform trying to save
-            callback(null, "platform", true, { "platform": "SamplePlatform", "otherConfig": "SomeData" });
-            return;
-        }
-
-        // - UI Type: Input
-        // Can be used to request input from user
-        // User response can be retrieved from request.response.inputs next time
-        // when configurationRequestHandler being invoked
-
-        var respDict = {
-            "type": "Interface",
-            "interface": "input",
-            "title": "Add Accessory",
-            "items": [
-                {
-                    "id": "name",
-                    "title": "Name",
-                    "placeholder": "Fancy Light"
-                }//, 
-                // {
-                //   "id": "pw",
-                //   "title": "Password",
-                //   "secure": true
-                // }
-            ]
-        }
-
-        // - UI Type: List
-        // Can be used to ask user to select something from the list
-        // User response can be retrieved from request.response.selections next time
-        // when configurationRequestHandler being invoked
-
-        // var respDict = {
-        //   "type": "Interface",
-        //   "interface": "list",
-        //   "title": "Select Something",
-        //   "allowMultipleSelection": true,
-        //   "items": [
-        //     "A","B","C"
-        //   ]
-        // }
-
-        // - UI Type: Instruction
-        // Can be used to ask user to do something (other than text input)
-        // Hero image is base64 encoded image data. Not really sure the maximum length HomeKit allows.
-
-        // var respDict = {
-        //   "type": "Interface",
-        //   "interface": "instruction",
-        //   "title": "Almost There",
-        //   "detail": "Please press the button on the bridge to finish the setup.",
-        //   "heroImage": "base64 image data",
-        //   "showActivityIndicator": true,
-        // "showNextButton": true,
-        // "buttonText": "Login in browser",
-        // "actionURL": "https://google.com"
-        // }
-
-        // Plugin can set context to allow it track setup process
-        context.ts = "Hello";
-
-        // Invoke callback to update setup UI
-        callback(respDict);
-    }
-    // Sample function to show how developer can add accessory dynamically from outside event
-    addAccessory(accessoryName) {
-        this.log("Add Accessory");
-        var platform = this;
-        var uuid;
-
-        uuid = UUIDGen.generate(accessoryName);
-
-        var newAccessory = new Accessory(accessoryName, uuid);
-        newAccessory.on('identify', function (paired, callback) {
-            platform.log(newAccessory.displayName, "Identify!!!");
-            callback();
-        });
-        // Plugin can save context on accessory to help restore accessory in configureAccessory()
-        // newAccessory.context.something = "Something"
-
-        // Make sure you provided a name for service, otherwise it may not visible in some HomeKit apps
-        newAccessory.addService(Service.WindowCovering, "Roller Blind")
+    getServices() {
+        let windowCoveringService = new Service.WindowCovering();
+        windowCoveringService
             .getCharacteristic(Characteristic.CurrentPosition)
-            .on('set', function (value, callback) {
-                platform.log(newAccessory.displayName, "Light -> " + value);
+            .on('get', (callback) => {
+                callback(null, 100);
+            })
+            .on('set', (value, callback) => {
+                this.log.info("CurrentPosition set called.");
                 callback();
             });
-
-        this.accessories.push(newAccessory);
-        this.api.registerPlatformAccessories("homebridge-samplePlatform", "SamplePlatform", [newAccessory]);
-    }
-    // Sample function to show how developer can remove accessory dynamically from outside event
-    removeAccessory() {
-        this.log("Remove Accessory");
-        this.api.unregisterPlatformAccessories("homebridge-samplePlatform", "SamplePlatform", this.accessories);
-
-        this.accessories = [];
+        this.windowCoveringService = windowCoveringService;
+        return [windowCoveringService];
     }
 }
